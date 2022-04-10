a9d31f94add8e9443698d96db3b5f0a6c52c558a
safeuer <sfeuer45@gmail.com>
2022-04-10 16:27:58 -0400

    Update App.js

--- music_project/music_frontend/src/App.js
+++ music_project/music_frontend/src/App.js
@@ -1,25 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeSong: {
+        title: "",
+        artist: ""
+      },
+      songList: []
+    };
+  }
+
+  componentDidMount() {
+    this.refreshSongs();
+  }
+
+  refreshSongs = () => {
+    axios
+      .get("http://localhost:8000/api/songs") // CHANGE
+      .then(result => this.setState({ songList: result.data }))
+      .catch(error => console.log(error))
+  }
 }
 
 export default App;
